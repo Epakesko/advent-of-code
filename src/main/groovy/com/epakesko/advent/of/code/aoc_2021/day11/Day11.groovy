dbf4290d9751c57e78c661926f75504c4daaf341
Bendegúz <bendeguz.takacs@gmail.com>
2022-12-07 15:49:50 +0100

    update gradle and groovy, comment out breaking code

--- src/main/groovy/com/epakesko/advent/of/code/aoc_2021/day11/Day11.groovy
+++ src/main/groovy/com/epakesko/advent/of/code/aoc_2021/day11/Day11.groovy
@@ -43,8 +43,9 @@ public class Day11 extends Day{
 		for(int i = -1; i <= 1; i++) {
 			for(int j = -1; j <= 1; j++) {
 				if(i != 0 || j != 0) {
-					Point n = new Point(((octopus.x) as Integer) + i, ((octopus.y) as Integer) + j)
-					if(map.containsKey(n)) neighbors << n
+					//TODO: fix
+					//Point n = new Point(((octopus.x) as Integer) + i, ((octopus.y) as Integer) + j)
+					//if(map.containsKey(n)) neighbors << n
 				}
 			}
 		}
