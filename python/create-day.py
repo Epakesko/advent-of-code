b257c9601a43b1a7a3f9ea717c51f2cb5f3e74e0
Bendegúz Takács <bendeguz.takacs@gmail.com>
2023-08-23 00:52:23 +0200

    handle virtual env w/ poetry, fix bugs

--- python/create-day.py
+++ python/create-day.py
@@ -1,19 +1,18 @@
 import requests
-import json
 import sys
 import os
 import re
 from bs4 import BeautifulSoup
 
 headers = { "user-agent": "github.com/Epakesko/advent-of-code by bendeguz.takacs@gmail.com" }
-cookies={'session': "53616c7465645f5f5fe2b845d799bdee83b2c5fb0f2112884d302ee52927fd4e158bd16e21c8ad5e5c4f143ec5ff11c7a0792f7bb2cce064fed9380ba0b7ce1a"}
+cookies={'session': os.environ.get("AOC_AUTH_TOKEN")}
 base_url = "https://adventofcode.com/2022/day/"
 day = sys.argv[1]
 
 # Create a new folder
 day_name = "day" + day.zfill(2)
 try:
-    os.mkdir("day_name")
+    os.mkdir(day_name)
 except FileExistsError:
     # If the folder already exists, ignore the error and continue
     pass
@@ -26,7 +25,7 @@ day_title = re.sub(r"--- Day \d+: (.*) ---", r"\1", h2.text)
 
 # Create input.txt
 response = requests.get(base_url + day + "/input", headers=headers, cookies=cookies)
-with open(os.path.join(day_name, "input.txt"), "w") as f:
+with open(os.path.join(day_name, "input.txt"), "w+") as f:
     f.write(response.text)
 
 # Create solution script based on the template
