3bbdf18d1f451e6505376fbd522454b1129eadbf
Bendegúz Takács <bendeguz.takacs@gmail.com>
2024-12-20 23:03:27 +0100

    2025 Day20 python part2 done

--- python/2025/day20/sol.py
+++ python/2025/day20/sol.py
@@ -17,7 +17,7 @@ for y, line in enumerate(lines):
             break
 
 road = {start: 0}
-road2 = [start]
+
 
 def get_next_step(prev, current):
     x, y = current
@@ -27,60 +27,29 @@ def get_next_step(prev, current):
         if neighbor != prev and xn >= 0 and xn < width and yn >= 0 and yn < height and (lines[yn][xn] == "." or neighbor == end):
             return neighbor
 
+
 prev = None
 current = start
 i = 1
-while(current != end):
+while (current != end):
     x, y = current
     next = get_next_step(prev, current)
     prev = current
     current = next
     road[current] = i
-    road2.append(current)
     i += 1
 
-def cheat(asd, to_):
-    x1 = asd[0]
-    y1 = asd[1]
-    x2, y2 = to_
-    steps_to_cheat = abs(x1 - x2) + abs(y1 - y2)
-    if steps_to_cheat < 51:
-        return steps_to_cheat
-    return 0
-
-max_cheat = 50
+max_cheat = 20
 min_diff = 100
-cheats = []
 c = 0
 print(i)
 for step in road:
-    for xd in range(-max_cheat, max_cheat + 1):
-        for yd in range(-(max_cheat - abs(xd) + 1), max_cheat - abs(xd) + 1):
-            x = step[0] + xd
-            y = step[1] + yd
-            if (x, y) in road and road[(x, y)] - road[step] - abs(xd) - abs(yd) >= min_diff:
-                c += 1
+    for xd in range(max_cheat + 1):
+        for yd in range(max_cheat - xd + 1):
+            options = {(xd, yd), (-xd, yd), (xd, -yd), (-xd, -yd)}
+            for option in options:
+                pos = (step[0] + option[0], step[1] + option[1])
+                if pos in road and road[pos] - road[step] - xd - yd >= min_diff:
+                    c += 1
+
 print(c)
-
-# for i in range(0, len(road2) - min_diff - 1):
-#     for j in range(i + min_diff, len(road2)):
-#         cheating_length = cheat(road2[i], road2[j])
-        #if cheating_length > 0 and j - i - cheating_length >= min_diff:
-         #    cheats.add((i, j))
-
-# lengt = len(cheats)
-# print(lengt)
-# print("validating...")
-# dic = {}
-# for cheat in cheats:
-#     i, j = cheat
-#     a = road[i]
-#     b = road[j]
-#     steps_to_cheat = abs(a[0] - b[0]) + abs(a[1] - b[1])
-#     dic.setdefault(j - i - steps_to_cheat, 0)
-#     dic[j - i - steps_to_cheat] += 1
-#     if i >= j or steps_to_cheat > 50 or j - i - steps_to_cheat < min_diff:
-#         print("invalid", i, j, steps_to_cheat)
-
-# print(cheats[-100:])
-
